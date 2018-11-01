2fab66a1b45a8c1df694527cf22b6f84e2b80064
ATUHAIRE JUDE INNOCENT <judeinno@ATUHAIREs-MacBook-Pro.local>
2018-11-01 18:20:53 +0300

    feat(Authentication): Password reset
    - Wrote tests for password endpoint.
    - Modify settings.py file to add send grid key.
    - Add URLs for endpoints for sending an email for POST-password reset and PUT-update password.
    - Add serializers for POST to send email for reset and PUT for resetting the password.
    - Add endpoints to POST-send email for password reset and a PUT-to reset password.
    - Update Registration endpoint to send verification email using send grid.
    - Refactoring tests to use a basetest file for all tests.
    - Refactoring sending of email to use one util.
    
    [Finishes #161348794]

--- authors/apps/authentication/tests/test_register.py
+++ authors/apps/authentication/tests/test_register.py
@@ -1,4 +1,4 @@
-from rest_framework.test import APITestCase
+from .basetest import BaseTestCase
 from rest_framework import status
 from django.contrib.auth import get_user_model
 from rest_framework.reverse import reverse
@@ -9,96 +9,47 @@ User = get_user_model()
 signup_url = reverse("authentication:signup")
 
 
-class UserApiTestCase(APITestCase):
-    def setUp(self):
-        self.user_data = {
-            "user": {
-                "email": "daniel@test.com",
-                "username": "daniel",
-                "password": "testpassword#123"
-            }
-        }
-        self.invalid_email_data = {
-            "user": {
-                "email": "test@testcom",
-                "username": "testuser",
-                "password": "testpassword1"
-            }
-        }
-        self.invalid_username = {
-            "user": {
-                "email": "test@test.com",
-                "username": "<@#!$%^$^",
-                "password": "testpassword"
-            }
-        }
-        self.invalid_pass_data = {
-            "user": {
-                "email": "test@test.com",
-                "username": "testuser",
-                "password": "123345678"
-            }
-        }
-        self.pass_less8_data = {
-            "user": {
-                "email": "test@test.com",
-                "username": "testuser",
-                "password": "12345l",
-            }
-        }
-        self.alp_pass_data = {
-            "user": {
-                "username": "jude",
-                "email": "jude@mail.com",
-                "password": "judesecret"
-            }
-        }
-
-        self.no_fields = {
-            "user": {
-                "username": "",
-                "email": "",
-                "password": "judesecret"
-            }
-        }
-        self.profile_data = {
-            "profiles": {
-                "username": "bruce",
-                "bio": "this is a test user",
-                "image": "",
-                "last_name": "",
-                "first_name": ""
-            }
-        }
+class UserApiTestCase(BaseTestCase):
 
     def test_register_user(self):
         self.response = self.client.post(
-            signup_url, self.user_data, format="json")
+            signup_url,
+            data={
+                "user": {
+                    "email": "judeinno@gmail.com",
+                    "username": "rachel",
+                    "password": "testpassword#123"
+                }
+            }, format="json")
         self.assertEqual(self.response.status_code, status.HTTP_201_CREATED)
 
     def test_invalid_email(self):
-        self.response = self.client.post(
-            signup_url, self.invalid_email_data, format="json")
+        self.response = self.client.post(signup_url,
+                                         self.invalid_email_data,
+                                         format="json")
         self.assertEqual(self.response.status_code,
                          status.HTTP_400_BAD_REQUEST)
         self.assertEqual(["Enter a valid email address."],
                          self.response.data["errors"]["email"])
 
     def test_register_with_invalid_username(self):
-        self.response = self.client.post(
-            signup_url, self.invalid_username, format="json")
+        self.response = self.client.post(signup_url,
+                                         self.invalid_username,
+                                         format="json")
         self.assertEqual(self.response.status_code,
                          status.HTTP_400_BAD_REQUEST)
 
     def test_register_with_invalid_password_numbers_only(self):
-        self.response = self.client.post(
-            signup_url, self.invalid_pass_data, format="json")
+        self.response = self.client.post(signup_url,
+                                         self.invalid_pass_data,
+                                         format="json")
         self.assertEqual(self.response.status_code,
                          status.HTTP_400_BAD_REQUEST)
 
     def test_password_length(self):
-        self.response = self.client.post(
-            signup_url, self.pass_less8_data, format="json")
+        self.response = self.client.post(signup_url,
+                                         self.pass_less8_data,
+                                         format="json")
         self.assertEqual(self.response.status_code,
                          status.HTTP_400_BAD_REQUEST)
         self.assertEqual(
@@ -107,14 +58,16 @@ class UserApiTestCase(APITestCase):
         )
 
     def test_password_not_alphanumeric(self):
-        self.response = self.client.post(
-            signup_url, self.alp_pass_data, format="json")
+        self.response = self.client.post(signup_url,
+                                         self.alp_pass_data,
+                                         format="json")
         self.assertEqual(self.response.status_code,
                          status.HTTP_400_BAD_REQUEST)
 
     def test_user_has_filled_in_Fields(self):
-        self.response = self.client.post(
-            signup_url, self.no_fields, format="json")
+        self.response = self.client.post(signup_url,
+                                         self.no_fields,
+                                         format="json")
         self.assertEqual(self.response.status_code,
                          status.HTTP_400_BAD_REQUEST)
         self.assertIn('This field may not be blank.',
